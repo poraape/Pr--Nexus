2b7c083940f452e1700a572c4838e6cb0619f044
poraape <medjotavesf@gmail.com>
2025-10-28 20:37:04 -0300

    sum

--- index.tsx
+++ index.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './src/index.css';
+import '@fontsource/inter';
+import './src/index.css';
+import '@fontsource/inter';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
