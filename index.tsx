2e38d747a022fd49cecf5d7895db612997c69873
poraape <medjotavesf@gmail.com>
2025-10-28 21:09:48 -0300

    IU

--- index.tsx
+++ index.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './src/index.css';
 import '@fontsource/inter';
-import './src/index.css';
-import '@fontsource/inter';
+import './src/styles/tailwind.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
